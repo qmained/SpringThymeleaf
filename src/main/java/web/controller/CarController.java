2837c67c642192a45544a9847be4d35e8d938166
qmained <unconfigured@null.spigotmc.org>
2023-04-26 17:52:25 +0300

    removed unused imports

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -2,9 +2,10 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
 
 @Controller
@@ -17,12 +18,6 @@ public class CarController {
     public CarController(CarService carService) {
         this.carService = carService;
     }
-//    @GetMapping()
-//    public String index(ModelMap model) {
-//        model.addAttribute("cars", carService.index());
-//
-//        return "cars/index";
-//    }
 
     @GetMapping()
     public String getCars(@RequestParam(name = "count") int count, ModelMap model) {
